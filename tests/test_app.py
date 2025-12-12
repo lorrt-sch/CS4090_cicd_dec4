0c8efd214a4b9a58f021da2b14b9df50b3c03a3a
lorrt-sch <lorrt@umsystem.edu>
2025-12-12 23:36:08 +0000

    attempting to fix app import in test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
-from src.app import add
+from app import add
 
 def test_add():
     assert add(5, 6) == 11
